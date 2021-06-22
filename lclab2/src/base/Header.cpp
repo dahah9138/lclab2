1fc46ec54a07dad575e325b87e163c8a8bb9a6fc
dahah9138 <daha9138@colorado.edu>
2021-06-21 19:00:17 -0600

    Fixed header system

--- lclab2/src/base/Header.cpp
+++ lclab2/src/base/Header.cpp
@@ -4,6 +4,9 @@ namespace LC {
 
 	void Header::write(const std::string& file) {
 
+		std::size_t size_t_in_bytes = sizeof(std::size_t);
+		std::size_t size_of_char_in_bytes = sizeof(char);
+
 		std::ofstream ofile(file.c_str(), std::ios::out | std::ios::binary);
 
 		if (!ofile) {
@@ -23,8 +26,21 @@ namespace LC {
 
 		// Write header information
 
-		for (const auto& obj : headerObjects) {
-			ofile.write((char*)&obj, sizeof(HeaderObject{}));
+		for (const auto& hObj : headerObjects) {
+
+			std::size_t variable_size = hObj.variable.size();
+
+			// Write size of variable name
+			ofile.write((char*)&variable_size, size_t_in_bytes);
+
+			// Read variable
+			ofile.write((char*)&hObj.variable[0], variable_size * size_of_char_in_bytes);
+
+			// Read byte size of obj
+			ofile.write((char*)&hObj.size_in_bytes, size_t_in_bytes);
+			
+			// Read location in body file
+			ofile.write((char*)&hObj.location, size_t_in_bytes);
 		}
 
 		ofile.close();
@@ -36,9 +52,12 @@ namespace LC {
 
 	void Header::read(const std::string& file) {
 
+		std::size_t size_t_in_bytes = sizeof(std::size_t);
+		std::size_t size_of_char_in_bytes = sizeof(char);
+
 		readFile = file;
 
-		std::ifstream ifile(file.c_str(), std::ios::out | std::ios::binary);
+		std::ifstream ifile(file.c_str(), std::ios::in | std::ios::binary);
 
 		if (!ifile) {
 			LC_WARN("Failed to open file <{0}>", file.c_str());
@@ -51,25 +70,33 @@ namespace LC {
 		// Read number of header objects
 		{
 			std::size_t numObj;
-			ifile.read((char*)&numObj, sizeof(std::size_t));
+			ifile.read((char*)&numObj, size_t_in_bytes);
+
 			headerObjects.resize(numObj);
 		}
 
 		// Read header information
 
-		{
-			std::size_t headerObjectsSizeInBytes = sizeof(HeaderObject) * headerObjects.size();
+		for (auto & hObj : headerObjects) {
 
-			std::vector<HeaderObject> objectBuffer;
-			objectBuffer.resize(headerObjects.size());
+			// Read size of variable name
+			std::size_t variable_size;
+			ifile.read((char*)&variable_size, size_t_in_bytes);
 
-			ifile.read((char*)&objectBuffer[0], headerObjectsSizeInBytes);
+			hObj.variable.resize(variable_size);
+
+			// Read variable
+			ifile.read((char*)&hObj.variable[0], variable_size * size_of_char_in_bytes);
+
+			// Read byte size of obj
+			ifile.read((char*)&hObj.size_in_bytes, size_t_in_bytes);
 			
-			// copy to headerObjects
-			for (int i = 0; i < headerObjects.size(); i++)
-				headerObjects[i] = objectBuffer[i];
+			// Read location in body file
+			ifile.read((char*)&hObj.location, size_t_in_bytes);
 		}
 		
+		LC_CORE_INFO("Here");
+
 		ifile.close();
 
 		if (!ifile.good()) {
